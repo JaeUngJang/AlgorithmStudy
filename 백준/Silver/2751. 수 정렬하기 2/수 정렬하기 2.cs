76b5b029c10e96d3d113104e40657aaf77639b7b
Jae Ung Jang <83953721+Jae-Ung-Code@users.noreply.github.com>
2022-07-25 21:07:28 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 536 ms, Memory: 69220 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cs
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cs
@@ -1,14 +1,14 @@
-using System.Text;
+using System.IO;
 
-StringBuilder sb = new StringBuilder();
-int T = int.Parse(Console.ReadLine());
-int[] nums = new int[T];
-for (int i = 0; i < T; i++)
-    nums[i] = int.Parse(Console.ReadLine());
+int n = int.Parse(Console.ReadLine());
+int[] m = new int[n];
+for (int i = 0; i < n; i++)
+{
+    m[i] = int.Parse(Console.ReadLine());
+}
 
-Array.Sort(nums);
-
-for (int i = 0; i < nums.Length; i++)
-    sb.AppendLine(nums[i].ToString());
-
-Console.WriteLine(sb);
+Array.Sort(m);
+using (var sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput())))
+{
+    sw.WriteLine(string.Join("\n", m));
+}
