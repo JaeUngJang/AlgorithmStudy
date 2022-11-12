dbd6e7a7d1bd4f55d0a995ec2a498570461bc6bc
Jae Ung Jang <83953721+JaeUngJang@users.noreply.github.com>
2022-11-12 22:04:22 +0900

    [Bronze II] Title: 피보나치 수, Time: 124 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Bronze/2747. 피보나치 수/피보나치 수.java
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -3,29 +3,30 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+    
+    public static int[] memo;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(br.readLine());
 
-        int[] fibo = new int[n + 1];
-        fibo = fibonacci(fibo);
-
-        sb.append(fibo[n]);
+        memo = new int[n];
+        sb.append(memo(n));
 
         System.out.println(sb);
     }
     
-    public static int[] fibonacci(int[] fibo) {
-        fibo[0] = 0;
-        fibo[1] = 1;
+    public static int memo(int n) {
+        if (n == 1) { memo[n-1] = 1; return 1; }
+        if (n == 2) { memo[n-1] = 1; return 1; }
 
-        for (int i = 2; i < fibo.length; i++) {
-            fibo[i] = fibo[i-2] + fibo[i-1];
+        if (memo[n-1] == 0) {
+            memo[n-1] = memo(n-1) + memo(n-2);
         }
 
-        return fibo;
+        return memo[n-1];
     }
 
 }
